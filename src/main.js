951a716062a6f95a3aebfc5f9c1f73a1a9e2070f
Emily Watkins and John Rykken <john@>
2018-03-19 15:07:11 -0700

    Add jQuery API.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { City } from './city.js';
 
 $(document).ready(function() {
+// API JQUERY
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=dda387c46c07145b7f41c73512df5121`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.")
+      }
+    });
+  });
 
+// PANDEMIC
   let portland;
   portland = new City("portland");
   portland.setInfection();
@@ -15,7 +35,7 @@ $(document).ready(function() {
   setInterval(function(){ $('#pdx-infect').text(portland.infectionLevel); }, 1000);
 
 
-  $('#pdx-treat').click(function(){
-
+  $('#pdx-treat').click(function(){ //do this after lunch
+    portland.treat(5);
   });
 })
