58b33adc8b6ddf51ded18e634d9e877c2838ebfc
Emily Watkins and John Rykken <emily@>
2018-03-20 13:22:02 -0700

    Rewrite API call as promise using JQuery then method.

--- src/main.js
+++ src/main.js
@@ -27,21 +27,32 @@ $(document).ready(function() {
   });
 
   // GIPHY
+  // $('#giphy-btn').click(function() {
+  //   const key = process.env.GIPHY_API_KEY;
+  //   $.ajax({
+  //     url: `http://api.giphy.com/v1/gifs/search?q=los_angeles&api_key=${key}&limit=5`,
+  //     type: 'GET',
+  //     data: {
+  //       format: 'json'
+  //     },
+  //     success: function(response) {
+  //       var reply = response.data[1].images.original.url;
+  //       console.log("Here is your reply", reply);
+  //       $('#giphy-span2').html('<img class="card-img-top"  src="' + reply + '">');
+  //     }
+  //   });
+  // });
+
+  // JQUERY PROMISE
 
   $('#giphy-btn').click(function() {
     const key = process.env.GIPHY_API_KEY;
-
-    $.ajax({
-      url: `http://api.giphy.com/v1/gifs/search?q=los_angeles&api_key=${key}&limit=5`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        var reply = response.data[1].images.original.url;
-        console.log("Here is your reply", reply);
-        $('#giphy-span2').html('<img class="card-img-top"  src="' + reply + '">');
-      }
+    $.get(`http://api.giphy.com/v1/gifs/search?q=los_angeles&api_key=${key}&limit=5`).then(function(response) {
+      var reply = response.data[1].images.original.url;
+      console.log("Here is your reply", reply);
+      $('#giphy-span2').html('<img class="card-img-top"  src="' + reply + '">');
+    }).fail(function(error) {
+      alert("You failed. You are a failure. You failed.")
     });
   });
 
